55f9a4e01a8105c595c110881bad2c830a64edbc
Echaftech23 <rachidchaf2001@gmail.com>
2023-11-29 22:58:37 +0100

    Set Movie Mingle Environement

---
+++ config/connection.php
@@ -0,0 +1,12 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "mingle_dashboard";    
+
+    $connexion = new mysqli($servername, $username, $password, $dbname);
+
+    if(isset($connexion->connect_error)){
+        die('error connection here '. $connexion->connect_error);
+    }
+?>
